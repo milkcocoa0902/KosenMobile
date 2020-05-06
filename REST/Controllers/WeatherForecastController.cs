238396aa0b40f59655cc17e2ac8c50240de6ea36
keita <koron0902@gmail.com>
2020-05-06 23:37:40 +0900

    RESTのTEST

--- REST/Controllers/WeatherForecastController.cs
+++ REST/Controllers/WeatherForecastController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-
+using System.Text.Json;
 namespace REST.Controllers
 {
     [ApiController]
@@ -36,4 +36,44 @@ namespace REST.Controllers
             .ToArray();
         }
     }
+    
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsDone { get; set; }
+    }
+
+    [ApiController]
+    [Route("api")]
+    public class ToController : ControllerBase
+    {
+        // Todoアイテムの初期データ。本来はデータベースなどから取得する。
+        private static List<TodoItem> items = new List<TodoItem>() {
+            new TodoItem() { Id = 1, Name = @"犬の散歩", IsDone = false },
+            new TodoItem() { Id = 2, Name = @"買い物", IsDone = true },
+            new TodoItem() { Id = 3, Name = @"本棚の修理", IsDone = false },
+        };
+
+        [HttpGet]
+        public ActionResult<List<TodoItem>> GetAll()
+            => items;
+
+        [HttpGet("{id}", Name = "Todo")]
+        public ActionResult<TodoItem> GetById(int id)
+        {
+            var item = items.Find(i => i.Id.Equals(id));
+            if (item == null)
+                return NotFound();
+            return item;
+        }
+
+        [HttpPost]
+        public ActionResult<TodoItem> GetJson([FromBody] TodoItem json){
+            Console.WriteLine(json);
+            //var weatherForecast = JsonSerializer.Deserialize<TodoItem>(json);
+            return json;
+        }
+    }
+    
 }
