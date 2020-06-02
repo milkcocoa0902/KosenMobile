4f960815621f994525b006685153f326559e3d4e
Keita <koron0902@gmail.com>
2020-06-02 23:47:24 +0900

    レイアウトの初期化が重いから全体的にBGスレッドに回した

--- MobileApp/syllabus/SyllabusActivity.cs
+++ MobileApp/syllabus/SyllabusActivity.cs
@@ -28,46 +28,57 @@ namespace KosenMobile.syllabus {
 
       // Create your application here
       SetContentView(Resource.Layout.syllabus_main_content);
-      sylabusDataManager_ = new syllabus.DataManager(ApplicationContext);
 
       sbjSpinner_ = FindViewById<Spinner>(Resource.Id.subject_item);
-      sbjList_ = sylabusDataManager_.gradeAndCourse(1, DataModel.Course.M);
-      sbjSpinner_.Adapter = new ArrayAdapter(this,
-        Resource.Layout.support_simple_spinner_dropdown_item,
-        sbjList_.Select(sbj => sbj.title_).ToList());
-        sbjSpinner_.SetSelection(0);
-
-
       gradeSpinner_ = FindViewById<Spinner>(Resource.Id.grade_item);
-      gradeSpinner_.Adapter = new ArrayAdapter(this,
-        Resource.Layout.support_simple_spinner_dropdown_item,
-        new int[]{
-          1, 2, 3, 4, 5
-      });
-      gradeSpinner_.SetSelection(0);
-
-      gradeSpinner_.ItemSelected += (sender, e) => {
-        sbjList_ = sylabusDataManager_.gradeAndCourse(gradeSpinner_.SelectedItemPosition + 1, (DataModel.Course)(new[] {1, 7, 3, 4, 5 }[courseSpinner_.SelectedItemPosition]));
-        sbjSpinner_.Adapter = new ArrayAdapter(this,
-          Resource.Layout.support_simple_spinner_dropdown_item,
-          sbjList_.Select(sbj => sbj.title_).ToList());
-      };
-
-
       courseSpinner_ = FindViewById<Spinner>(Resource.Id.course_item);
-      courseSpinner_.Adapter = new ArrayAdapter(this,
-        Resource.Layout.support_simple_spinner_dropdown_item,
-        new DataModel.Course[]{
-          DataModel.Course.M, DataModel.Course.E, DataModel.Course.I, DataModel.Course.C, DataModel.Course.A
-      });
-      courseSpinner_.SetSelection(0);
 
-      courseSpinner_.ItemSelected += (sender, e) => {
-        sbjList_ = sylabusDataManager_.gradeAndCourse(gradeSpinner_.SelectedItemPosition + 1, (DataModel.Course)(new[] { 1, 7, 3, 4, 5 }[courseSpinner_.SelectedItemPosition]));
-        sbjSpinner_.Adapter = new ArrayAdapter(this,
-          Resource.Layout.support_simple_spinner_dropdown_item,
-          sbjList_.Select(sbj => sbj.title_).ToList());
-      };
+      new System.Threading.Tasks.Task(() => {
+        sylabusDataManager_ = new syllabus.DataManager(ApplicationContext);
+
+        sbjList_ = sylabusDataManager_.gradeAndCourse(1, DataModel.Course.M);
+
+        RunOnUiThread(() => {
+          sbjSpinner_.Adapter = new ArrayAdapter(this,
+            Resource.Layout.support_simple_spinner_dropdown_item,
+            sbjList_.Select(sbj => sbj.title_).ToList());
+          sbjSpinner_.SetSelection(0);
+        });
+
+
+
+        RunOnUiThread(() => {
+          gradeSpinner_.Adapter = new ArrayAdapter(this,
+            Resource.Layout.support_simple_spinner_dropdown_item,
+            new int[]{
+          1, 2, 3, 4, 5
+          });
+          gradeSpinner_.SetSelection(0);
+        });
+
+        gradeSpinner_.ItemSelected += (sender, e) => {
+          sbjList_ = sylabusDataManager_.gradeAndCourse(gradeSpinner_.SelectedItemPosition + 1, (DataModel.Course)(new[] { 1, 7, 3, 4, 5 }[courseSpinner_.SelectedItemPosition]));
+          sbjSpinner_.Adapter = new ArrayAdapter(this,
+            Resource.Layout.support_simple_spinner_dropdown_item,
+            sbjList_.Select(sbj => sbj.title_).ToList());
+        };
+
+        RunOnUiThread(() => {
+          courseSpinner_.Adapter = new ArrayAdapter(this,
+            Resource.Layout.support_simple_spinner_dropdown_item,
+            new DataModel.Course[]{
+          DataModel.Course.M, DataModel.Course.E, DataModel.Course.I, DataModel.Course.C, DataModel.Course.A
+          });
+          courseSpinner_.SetSelection(0);
+        });
+
+        courseSpinner_.ItemSelected += (sender, e) => {
+          sbjList_ = sylabusDataManager_.gradeAndCourse(gradeSpinner_.SelectedItemPosition + 1, (DataModel.Course)(new[] { 1, 7, 3, 4, 5 }[courseSpinner_.SelectedItemPosition]));
+          sbjSpinner_.Adapter = new ArrayAdapter(this,
+            Resource.Layout.support_simple_spinner_dropdown_item,
+            sbjList_.Select(sbj => sbj.title_).ToList());
+        };
+      }).Start();
     }
   }
 }
